f651df0b438f9465e89542adee278007862f6210
Callionica <16260283+callionica@users.noreply.github.com>
2020-10-03 09:59:56 +0100

    spread

--- src/utility.ts
+++ src/utility.ts
@@ -11,6 +11,19 @@ function isAsyncIterable(value: unknown): value is AsyncIterable<unknown> {
     return ((value as any)[Symbol.asyncIterator] != undefined);
 }
 
+async function* toAsyncIterable_<T>(iterable: Iterable<T>): AsyncIterable<T> {
+    for (const item of iterable) {
+        yield item;
+    }
+}
+
+function toAsyncIterable<T>(iterable: AnyIterable<T>): AsyncIterable<T> {
+    if (isAsyncIterable(iterable)) {
+        return iterable;
+    }
+    return toAsyncIterable_(iterable);
+}
+
 /** Converts an iterable or iterator into its value type */
 export type ValueOfIterable<T> =
     T extends Iterable<infer V0> ? V0 :
@@ -50,7 +63,7 @@ type Reverse<Input extends unknown[], Result extends unknown[] = []> = {
 /**
  * Takes in a list of iterable types
  * and returns a list of value types.
- */ 
+ */
 type ValuesOfIterables<Input extends unknown[], Result extends unknown[] = []> = {
     'Continue': ValuesOfIterables<Tail<Input>, Prepend<(ValueOfIterable<Head<Input>>), Result>>,
     'Return': Reverse<Result>
@@ -172,9 +185,31 @@ export async function arrayFrom<T>(iterable: AnyIterable<T>) {
 }
 
 /**
+ * If the iterable has a non-zero `length` property, returns the value of the `length` property.
+ * Otherwise iterates through the list and counts the values in the list.
+ * This is potentially an expensive operation!
+ * 
+ * @param iterable The list from which to obtain the length
+ */
+export async function length<T>(iterable: AnyIterable<T>): Promise<number> {
+    // deno-lint-ignore no-explicit-any
+    let length: number = (iterable as any)?.length || 0;
+    if (length > 0) {
+        return length;
+    }
+
+    for await (const item of iterable) {
+        ++length;
+    }
+
+    return length;
+}
+
+/**
  * Returns a value derived from the first item in the collection that matches a provided test.
  * 
- * Like Array.some except doesn't just return a boolean, or like Array.find except doesn't just return exactly the found item.
+ * Like Array.some except this function doesn't just return a boolean.
+ * Like Array.find except this functon doesn't just return exactly the found item.
  * 
  * @param iterable A collection of items
  * @param testAndMap A function that returns a relevant value when the provided item matches some criteria (and otherwise returns undefined).
@@ -187,4 +222,55 @@ export async function first<Item, Result>(iterable: AnyIterable<Item>, testAndMa
         }
     }
     return result;
+}
+
+/**
+ * Returns a new list consisting of the original list's values interspersed with filler
+ * values so that the new list has more items than the original.
+ * 
+ * The function works with iterables so it is lazy, but it requires the caller to already
+ * know the number of items in the original list which must be stable for the function to work.
+ * 
+ * @param iterable The list of values to be spread out
+ * @param iterableLength The current number of values in the list
+ * @param newLength The desired length of the new list
+ * @param filler The value used to spread out the list to the desired length
+ */
+export async function* spread<Item, Filler>(iterable: AnyIterable<Item>, iterableLength: number, options: { newLength: number, filler: Filler }): AsyncIterable<Item | Filler> {
+    const fillerLength = options.newLength - iterableLength;
+    if (fillerLength < 0) {
+        throw "spread can only extend length";
+    }
+
+    let fillerCount = 0;
+    let valueCount = 0;
+
+    for await (const item of iterable) {
+        yield item;
+        ++valueCount;
+
+        // We want to start and end with a real value
+        // so we always produce a real value first then,
+        // to determine whether to produce a filler value, we test
+        // the current progress of the iterator for length - 1
+        // against the progress the filler would have if we were to produce the filler value.
+        // If producing a filler value wouldn't make filler progress greater than
+        // the iterator progress, we produce a filler value.
+        // (By 'progress' we mean what proportion of the total number of values of each
+        // type we have produced).
+
+        while ((fillerCount < fillerLength) && ((valueCount / (iterableLength - 1)) >= ((fillerCount + 1) / fillerLength))) {
+            yield options.filler;
+            ++fillerCount;
+        }
+    }
+
+    // This is here through an abundance of caution.
+    // It could help if the iterable somehow produces fewer values than expected
+    // as it would ensure that the new list comes out at the expected length, but clearly,
+    // in that case, the distribution of real values and filler values would not be correct.
+    while (fillerCount < fillerLength) {
+        yield options.filler;
+        ++fillerCount;
+    }
 }
