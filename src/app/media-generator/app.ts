7339df0ba77d70d1b4ec96693c89b3b388fd43b9
Callionica <16260283+callionica@users.noreply.github.com>
2020-10-20 15:27:35 +0100

    app pageGroups

--- src/app/media-generator/app.ts
+++ src/app/media-generator/app.ts
@@ -3,6 +3,7 @@ import { loadEntry } from "../../junction.ts";
 import { getMediaGroups, MediaPrimary } from "../../media.ts";
 import { arrayFrom } from "../../utility.ts";
 import { pageGroup } from "./page-group.ts";
+import { pageGroups } from "./page-groups.ts";
 
 const entry = await loadEntry("/Volumes/A128/TV.junction");
 const mediaEntry = new MediaPrimary(entry);
@@ -11,9 +12,17 @@ const mediaGroups = await getMediaGroups(all);
 
 const destination = "/Volumes/WD01/_current/_tmp_/";
 
-// await writeFile(destination + "hello.txt", new Uint8Array([68,69,70]));
+{
+    const pageLocation = destination;
+    console.log(pageLocation);
+    await Deno.mkdir(pageLocation, { recursive: true });
 
-// throw "";
+    const page = await pageGroups(mediaGroups);
+    // console.log(page);
+
+    const pageData = new TextEncoder().encode(page);
+    await writeFile(pageLocation + "index.html", pageData);
+}
 
 for (const mediaGroup of mediaGroups) {
     
