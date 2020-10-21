21a0023b3bfc9a6725148cf9ff62ed6f816c26aa
Callionica <16260283+callionica@users.noreply.github.com>
2020-10-21 12:47:40 +0100

    WIP

--- src/app/media-generator/page-video.ts
+++ src/app/media-generator/page-video.ts
@@ -1,15 +1,20 @@
-import {
-    MediaGroup, MediaPrimary,
-} from "../../media.ts";
+import { readFile, readTextFile } from "../../file.ts";
+import { MediaGroup, MediaPrimary } from "../../media.ts";
 
 // Also handles audio
 export async function pageVideo(mediaGroup: MediaGroup, file: MediaPrimary) {
 
-    const images = await file.images();
-    const descriptions = await file.descriptions();
-    const subtitles = await file.subtitles();
+    const description = await file.description();
 
-    return file.urlName;
+    const images = await file.images();
+    const image = images[0]?.target; // TODO
+
+    const allSubtitles = await file.subtitles();
+    const subtitles = allSubtitles.filter(s => s.extension === "vtt").map(s => ({ url: s.target, mimetype: s.mimetype, language: s.language })); // TODO
+
+    const media = { url: file.target, mimetype: file.mimetype };
+
+    return file.urlName + "\n" + description;
 }
 
 // function html_source(vid, baseURL: URL) {
