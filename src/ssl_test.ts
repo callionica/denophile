019bc88f45d15bbfd54bce6edf6e9fc78a1b1332
Callionica <16260283+callionica@users.noreply.github.com>
2020-11-03 16:20:12 +0000

    ssl test

--- src/ssl_test.ts
+++ src/ssl_test.ts
@@ -1,11 +1,27 @@
+import { writeTextFile } from "./file.ts";
 import { SSL } from "./ssl.ts";
 
 
 Deno.test("ssl", async function () {
+    console.log("");
+
     const ssl = new SSL();
 
-    const cert = await ssl.fetchPin(new URL("https://main-hub.local"));
-    // const cert = await ssl.downloadCertificate(new URL("https://main-hub.local"));
+    const cert = await ssl.fetchCertificate(new URL("https://main-hub.local"));
 
+    const fn = "/Users/user/Desktop/__current/--1--1--dump.txt";
+    await writeTextFile(fn, cert);
+
+    const pin = await ssl.getPin(fn);
+
+    const subject = await ssl.getSubject(fn);
+
+    console.log(subject);
+    console.log(subject.CN);
     console.log(cert);
+    console.log(pin);
+
+    if (pin != "KCeZkuOUT+zijAND9jsWsIZk3NXdYs7jmm3U+T2J0Q4=") {
+        throw "FAIL";
+    }
 });
